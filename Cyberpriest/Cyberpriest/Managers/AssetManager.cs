9be6eaaa3deca7295e3c33c2ffebccb1e1a68b54
Emmy <vuongemmy@gmail.com>
2021-05-07 14:13:35 +0200

    Code Review_K02

--- Cyberpriest/Cyberpriest/Managers/AssetManager.cs
+++ Cyberpriest/Cyberpriest/Managers/AssetManager.cs
@@ -11,25 +11,8 @@ namespace Cyberpriest
 {
     class AssetManager
     {
-        public static Texture2D player;
-        public static Texture2D platform;
-        public static Texture2D bg;
-        public static Texture2D inventoryBG;
-        public static Texture2D item;
-        public static Texture2D walltile;
-        public static Texture2D enemy1;
-        public static Texture2D enemy2;
-        public static Texture2D enemy3;
-        public static Texture2D boss1;
-        public static Texture2D boss2;
-        public static Texture2D purpleFire;
-        public static Texture2D redFire;
-        public static Texture2D shield1;
-        public static Texture2D shield2;
-        public static Texture2D heartSprite;
-        public static Texture2D coinSprite;
-        public static Texture2D tilesetSprite;
-        public static Texture2D tilesetSprite2;
+        #region Platform
+
         public static Texture2D smallVTile;
         public static Texture2D largeVTile;
         public static Texture2D smallPlatform;
@@ -37,6 +20,35 @@ namespace Cyberpriest
         public static Texture2D tallPlatform;
         public static Texture2D squarePlatform;
         public static Texture2D longPlatform;
+
+        #endregion
+
+        #region Player
+
+        public static Texture2D player;
+
+        #endregion
+
+        #region Enemy
+
+        public static Texture2D enemyGhost;
+        public static Texture2D enemyDemon;
+        public static Texture2D enemySkeleton;
+        public static Texture2D bossCleopatra;
+        public static Texture2D bossCerberus;
+
+        #endregion
+
+        #region Stationary Enemy
+
+        public static Texture2D purpleFire;
+        public static Texture2D redFire;
+
+        #endregion
+
+        #region PowerUp
+
+        public static Texture2D item;
         public static Texture2D redWhitepil;
         public static Texture2D orangeBlackPil;
         public static Texture2D magnet;
@@ -61,40 +73,57 @@ namespace Cyberpriest
         public static Texture2D rubyEye;
         public static Texture2D diamond;
         public static Texture2D diamond2;
+
+        #endregion
+
+        #region Armor
+
         public static Texture2D silverBoots;
         public static Texture2D silverChestplate;
         public static Texture2D silverHelmet;
         public static Texture2D silverPants;
+
+        #endregion
+
+        #region Weapon
+
         public static Texture2D bloodySword;
         public static Texture2D blueSword;
         public static Texture2D redSword;
-        public static Texture2D pistol1;
-        public static Texture2D pistol2;
-        public static Texture2D pistol3;
-        public static Texture2D background1;
+        public static Texture2D semiAutomaticPistolVer1;
+        public static Texture2D semiAutomaticPistolVer2; 
+        public static Texture2D semiAutomaticPistolVer3; 
+        public static Texture2D shieldBlue;
+        public static Texture2D shieldRed;
+
+        #endregion
+
+        #region Background
+
+        //public static Texture2D inventoryBG;
+        public static Texture2D backgroundLvl1;
         public static Texture2D inventorySlot;
+
+        #endregion 
+
+        #region HUD
+
+        public static Texture2D heartSprite;
+        public static Texture2D coinSprite;
+
+        #endregion
+
+        #region Font
 
         public static SpriteFont normalFont;
         public static SpriteFont selectedFont;
 
+        #endregion
+
         public static void LoadAssets(ContentManager content)
         {
-            player= content.Load<Texture2D>("player1");
-            platform= content.Load<Texture2D>("plattform");
-            bg= content.Load<Texture2D>("background");
-            inventoryBG= content.Load<Texture2D>("hole");
-            item= content.Load<Texture2D>("PotionsSprite");          
-            enemy1 = content.Load<Texture2D>("Enemy1");
-            enemy2 = content.Load<Texture2D>("enemy2");
-            enemy3 = content.Load<Texture2D>("enemy3");
-            purpleFire = content.Load<Texture2D>("PurpleFireSprite");
-            redFire = content.Load<Texture2D>("RedFireSprite");
-            shield1 = content.Load<Texture2D>("Shield1");
-            shield2 = content.Load<Texture2D>("Shield2");
-            heartSprite = content.Load<Texture2D>("HeartSprite");
-            coinSprite = content.Load<Texture2D>("CoinSprite");
-            tilesetSprite = content.Load<Texture2D>("BrickTilesetSprite");
-            tilesetSprite2 = content.Load<Texture2D>("BrickTilesetSprite2");
+            #region Platfrom
+
             smallVTile = content.Load<Texture2D>("SmallVTile");
             largeVTile = content.Load<Texture2D>("LargeVTile");
             smallPlatform = content.Load<Texture2D>("SmallPlatform");
@@ -102,6 +131,34 @@ namespace Cyberpriest
             tallPlatform = content.Load<Texture2D>("TallPlatform");
             squarePlatform = content.Load<Texture2D>("SquarePlatform");
             longPlatform = content.Load<Texture2D>("LongPlatform");
+
+            #endregion
+
+            #region Player
+
+            player = content.Load<Texture2D>("player1");
+
+            #endregion
+
+            #region Enemy
+         
+            enemyGhost = content.Load<Texture2D>("Enemy1");
+            enemyDemon = content.Load<Texture2D>("enemy2");
+            enemySkeleton = content.Load<Texture2D>("enemy3");
+            bossCleopatra = content.Load<Texture2D>("Boss_Lust");
+            bossCerberus = content.Load<Texture2D>("Boss_Gluttony");
+
+            #endregion
+
+            #region Stationary Enemy
+
+            purpleFire = content.Load<Texture2D>("PurpleFireSprite");
+            redFire = content.Load<Texture2D>("RedFireSprite");
+
+            #endregion
+
+            #region PowerUp
+
             redWhitepil = content.Load<Texture2D>("l0_sprite_01");
             orangeBlackPil = content.Load<Texture2D>("l0_sprite_02");
             magnet = content.Load<Texture2D>("l0_sprite_03");
@@ -126,23 +183,53 @@ namespace Cyberpriest
             rubyEye = content.Load<Texture2D>("l0_sprite_22");
             diamond = content.Load<Texture2D>("l0_sprite_23");
             diamond2 = content.Load<Texture2D>("l0_sprite_24");
+            item = content.Load<Texture2D>("PotionsSprite");
+
+            #endregion
+
+            #region Armor
+
             silverBoots = content.Load<Texture2D>("Armor_Boots");
             silverChestplate = content.Load<Texture2D>("Armor_Chestplate");
             silverHelmet = content.Load<Texture2D>("Armor_Helmet");
             silverPants = content.Load<Texture2D>("Armor_Pants");
-            boss1 = content.Load<Texture2D>("Boss_Lust");
-            boss2 = content.Load<Texture2D>("Boss_Gluttony");
+
+            #endregion
+
+            #region Weapon
+
             bloodySword = content.Load<Texture2D>("Bloody_Sword");
             blueSword = content.Load<Texture2D>("Blue_Sword");
             redSword = content.Load<Texture2D>("Red_Sword");
-            pistol1 = content.Load<Texture2D>("Gun_Ver1");
-            pistol2 = content.Load<Texture2D>("Gun_Ver2");
-            pistol3 = content.Load<Texture2D>("Gun_Ver3");
-            background1 = content.Load<Texture2D>("BG_Lvl_1");
+            semiAutomaticPistolVer1 = content.Load<Texture2D>("Gun_Ver1");
+            semiAutomaticPistolVer2 = content.Load<Texture2D>("Gun_Ver2");
+            semiAutomaticPistolVer3 = content.Load<Texture2D>("Gun_Ver3");
+            shieldBlue = content.Load<Texture2D>("Shield1");
+            shieldRed = content.Load<Texture2D>("Shield2");
+
+            #endregion
+
+            #region Background
+
+            //inventoryBG = content.Load<Texture2D>("hole");
+            backgroundLvl1 = content.Load<Texture2D>("BG_Lvl_1");
             inventorySlot = content.Load<Texture2D>("InventorySlot");
 
+            #endregion 
+
+            #region HUD
+
+            heartSprite = content.Load<Texture2D>("HeartSprite");
+            coinSprite = content.Load<Texture2D>("CoinSprite");
+
+            #endregion
+
+            #region Font
+
             normalFont = content.Load<SpriteFont>(@"Font\normalFont");
-            selectedFont= content.Load<SpriteFont>(@"Font\selectedFont");
+            selectedFont = content.Load<SpriteFont>(@"Font\selectedFont");
+
+            #endregion
         }
     }
 }
